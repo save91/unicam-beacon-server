78cc9fd19e37436feb245dea7a7ae3340e948e1e
Marco D'Argenio <marco.dargenio@studenti.unicam.it>
2016-03-14 12:03:23 +0100

    grafica utenti senza azioni

--- webserver/public/js/controllers/utenti.js
+++ webserver/public/js/controllers/utenti.js
@@ -2,44 +2,44 @@
 angular.module('beaconApp.controllers.utenti', [])
 
 .controller('UtentiCtrl', function($scope, $location, Utenti) {
-  $scope.user = [];
+  $scope.users = [];
 
   var updatedUser = function() {
     Utenti.getAll().then(function(res){
-      $scope.user = res.data;
+      $scope.users = res.data;
     },
     function (res) {
       alert (res.data);
     }
   );
 };
-  $scope.createUser = function () {
-    Utenti.createUser($scope.user).then(
-      function(res) {
-        updateUser();
-      },
-      function (res){
-        alert (res.data);
-      }
-    );
-    $scope.user.username = "";
-    $scope.user.firstename = "";
-    $scope.user.lastaname = "";
-    $scope.user.permission = 10;
-    $scope.user.block = true;
-  }
-  $scope.updatedUser = function () {
-      updatedUser();
-  };
-  $scope.checkUsername = function () {
-    Utenti.checkUsername($scope.user).then(
-      function(res) {
-        updateUser();
-      },
-      function (res){
-        alert (res.data);
-      }
-    )
-  };
+$scope.createUser = function () {
+  Utenti.createUser($scope.users).then(
+    function(res) {
+      updateUser();
+    },
+    function (res){
+      alert (res.data);
+    }
+  );
+  $scope.user.username = "";
+  $scope.user.firstename = "";
+  $scope.user.lastaname = "";
+  $scope.user.permission = 10;
+  $scope.user.block = true;
+}
+$scope.updatedUser = function () {
   updatedUser();
+};
+$scope.checkUsername = function () {
+  Utenti.checkUsername($scope.users).then(
+    function(res) {
+      updateUser();
+    },
+    function (res){
+      alert (res.data);
+    }
+  )
+};
+updatedUser();
 })
