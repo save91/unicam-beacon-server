f6cf318b0d4447b461f908974a70a8ae6b126c8b
Saverio Tosi <saveriotosi@gmail.com>
2016-04-13 16:27:46 +0200

    handler error files

--- webserver/models/datamanager.js
+++ webserver/models/datamanager.js
@@ -13,9 +13,18 @@ datamanager.get_settings = function (req, res, next) {
   fs.readFile(SETTING_FILE, function(err, data) {
     if (err) {
       console.error(err);
-      process.exit(1);
     }
-    req.settings = JSON.parse(data);
+    try {
+      req.settings = JSON.parse(data) || [];
+    } catch (e) {
+      req.settings = {
+        "dhcp": false,
+        "ip": "127.0.0.1",
+        "netmask": "255.255.255.255",
+        "version": "1.0.0",
+        "name": "ProximitySystem"
+      };
+    }
     next();
   });
 };
@@ -24,7 +33,6 @@ datamanager.set_settings = function (req, res) {
   fs.writeFile(SETTING_FILE, JSON.stringify(req.settings, null), function(err) {
     if (err) {
       console.error(err);
-      process.exit(1);
     }
   });
 };
@@ -33,9 +41,20 @@ datamanager.get_users = function (req, res, next) {
   fs.readFile(USERS_FILE, function(err, data) {
     if (err) {
       console.error(err);
-      process.exit(1);
     }
-    req.users = JSON.parse(data);
+    try {
+      req.users = JSON.parse(data) || [];
+    } catch (e) {
+      req.users = [{
+        "username":"save",
+        "firstname":"Saverio",
+        "lastname":"Tosi",
+        "password":"123456",
+        "photo":"img/account.jpg",
+        "permission":0,
+        "block":false
+      }];
+    }
     next();
   });
 };
@@ -44,7 +63,6 @@ datamanager.set_users = function (req, res) {
   fs.writeFile(USERS_FILE, JSON.stringify(req.users, null), function(err) {
     if (err) {
       console.error(err);
-      process.exit(1);
     }
   });
 };
@@ -53,9 +71,12 @@ datamanager.get_beacons = function (req, res, next) {
   fs.readFile(BEACONS_FILE, function(err, data) {
     if (err) {
       console.error(err);
-      process.exit(1);
     }
-    req.beacons = JSON.parse(data);
+    try {
+      req.beacons = JSON.parse(data) || [];
+    } catch (e) {
+      req.beacons = [];
+    }
     next();
   });
 };
@@ -64,7 +85,6 @@ datamanager.set_beacons = function (req, res, next) {
   fs.writeFile(BEACONS_FILE, JSON.stringify(req.beacons, null), function(err) {
     if (err) {
       console.error(err);
-      process.exit(1);
     }
   });
 };
@@ -73,9 +93,12 @@ datamanager.get_devices = function (req, res, next) {
   fs.readFile(DEVICE_FILE, function(err, data) {
     if (err) {
       console.error(err);
-      process.exit(1);
     }
-    req.devices = JSON.parse(data);
+    try {
+      req.devices = JSON.parse(data) || [];
+    } catch (e) {
+      req.devices = [];
+    }
     next();
   });
 };
@@ -84,7 +107,6 @@ datamanager.set_devices = function (req, res, next) {
   fs.writeFile(DEVICE_FILE, JSON.stringify(req.devices, null), function(err) {
     if (err) {
       console.error(err);
-      process.exit(1);
     }
   });
 };
@@ -95,9 +117,19 @@ datamanager.get_io = function (req, res, next) {
   fs.readFile(IO_FILE, function(err, data) {
     if (err) {
       console.error(err);
-      process.exit(1);
     }
-    req.io = JSON.parse(data);
+    try {
+      req.io = JSON.parse(data) || [];
+    } catch (e) {
+      req.io = [
+        {"id":"1","type":"input","name":"Pulsante"},
+        {"id":"2","type":"input","name":"Orologio"},
+        {"id":"3","type":"input","name":"Fotocellula"},
+        {"id":"4","type":"output","name":"Cancello"},
+        {"id":"5","type":"output","name":"Apriporta"},
+        {"id":"6","type":"output","name":"Lampada"}
+      ];
+    }
     next();
   });
 };
@@ -117,9 +149,19 @@ datamanager.get_gpio = function (req, res, next) {
   fs.readFile(GPIO_FILE, function(err, data) {
     if (err) {
       console.error(err);
-      process.exit(1);
     }
-    req.gpio = JSON.parse(data);
+    try {
+      req.gpio = JSON.parse(data) || [];
+    } catch (e) {
+      req.gpio = [
+        {
+          "id":1,
+          "type":"output",
+          "GPIO":3,"id_device":0,
+          "state":true,"value":1
+        },
+        {"id":2,"type":"output","GPIO":5,"id_device":0,"state":0,"value":1},{"id":3,"type":"output","GPIO":7,"id_device":0,"state":0,"value":1},{"id":4,"type":"output","GPIO":11,"id_device":0,"value":1},{"id":5,"type":"output","GPIO":13,"id_device":0,"state":true,"value":1},{"id":6,"type":"output","GPIO":15,"id_device":0,"state":true,"value":1},{"id":7,"type":"output","GPIO":19,"id_device":0,"state":true, "value":1},{"id":8,"type":"output","GPIO":21,"id_device":0,"state":true,"value":1}];
+    }
     next();
   });
 };
@@ -130,7 +172,6 @@ datamanager.set_gpio = function (req, res, next) {
   fs.writeFile(GPIO_FILE, JSON.stringify(req.gpio, null), function(err) {
     if (err) {
       console.error(err);
-      process.exit(1);
     }
   });
 };
