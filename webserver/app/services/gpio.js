e79ad5bdf3dae8c58ef435fa2dc79263d4a31bc0
Saverio Tosi <saveriotosi@gmail.com>
2016-08-21 17:07:40 +0200

    test

--- webserver/app/services/gpio.js
+++ webserver/app/services/gpio.js
@@ -67,7 +67,7 @@ gpio.init = function (environment) {
     });
   }
   GPIO.update({type: 'output'},
-        {value: false},
+        {value: true},
         {multi: true},
       function(err, numAffected) {});
   GPIO.update({type: 'input'},
@@ -87,6 +87,7 @@ gpio.init = function (environment) {
                 console.log("Error opening pin " + err);
                 return;
               }
+              RPI_GPIO.write(GPIOs[i].GPIO, true, function () {});
             });
           }
         }else if(GPIOs[i].type==="input"){
