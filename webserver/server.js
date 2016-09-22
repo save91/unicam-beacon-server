c1c7fed66777bdda43032dc1e75ad4e00e939828
Saverio Tosi <saveriotosi@gmail.com>
2016-09-22 17:11:35 +0200

    removed business login of socket.io

--- webserver/server.js
+++ webserver/server.js
@@ -10,7 +10,7 @@ var beaconRoutes = require('./app/routes/beacon');
 var settingRoutes = require('./app/routes/setting');
 var gpioRoutes = require('./app/routes/gpio');
 
-var gpios = require('./app/services/gpio');
+var gpio = require('./app/services/gpio');
 
 var db = require('./config/db');
 var security = require('./config/security')
@@ -29,10 +29,6 @@ var morgan = require('morgan');
 var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
 
 var port = 8000;
-
-var GPIO = require('./app/models/gpio');
-var pin = require('./app/services/gpio');
-
 mongoose.connect(db.url);
 
 app.use(express.static(__dirname + '/angular'));
@@ -40,14 +36,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
 // middleware authentication & log
 app.use([userRoutes.authentication, morgan('common', {stream: accessLogStream})]);
 
-userRoutes.addAPIRouter(app);
-deviceRoutes.addAPIRouter(app, environment);
-beaconRoutes.addAPIRouter(app);
-settingRoutes.addAPIRouter(app);
-gpioRoutes.addAPIRoutes(app, environment);
+userRoutes.addAPIRouter(app, io);
+deviceRoutes.addAPIRouter(app, io, environment);
+beaconRoutes.addAPIRouter(app, io);
+settingRoutes.addAPIRouter(app, io);
+gpioRoutes.addAPIRoutes(app, io, environment);
 
 app.use(function(req, res, next){
   res.status(404);
@@ -66,61 +66,10 @@ app.use(function (err, req, res, next) {
 //catches ctrl+c event
 process.on('SIGINT', function(){
   console.log("Stop webserver");
-  gpios.unexportPins(environment);
+  gpio.unexportPins(environment);
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-
-  socket.on('get:gpio', function(){
-    GPIO.find(function(err, GPIOs) {
-      if(err) {
-        console.log('Errore');
-        socket.emit('error', {msg: err.errmsg});
-      } else if(GPIOs) {
-        console.log('Successo');
-        socket.emit('get:gpio', GPIOs);
-      }
-    });
-  });
-
-  socket.on('put:gpio', function(data){
-    GPIO.findOne({
-      '_id': data.id,
-      'type':'output'
-    }, function(err, gpio) {
-      if(err) {
-        socket.emit('error',{msg: err.errmsg});
-      } else if(gpio) {
-        if(undefined != data.value) {
-          gpio.value = data.value;
-        }
-        gpio.save(function (err) {
-          if(err) {
-            socket.emit('error',{msg: err.errmsg});
-          } else {
-            pin.setPin(gpio.GPIO, gpio.value, function(err) {
-              if (err) {
-                socket.emit('error', {msg: err.errmsg});
-              } else {
-                socket.broadcast.emit('put:gpio', gpio);
-              }
-            }, environment);
-          }
-        });
-      } else {
-        socket.emit('error', {msg: err.errmsg});
-      }
-    });
-  });
-});
-
-
-
-gpios.init(environment);
+gpio.init(environment);
 http.listen(port ,function() {
   console.log('GPIO setup completed and server listening on port ' + port);
 });
